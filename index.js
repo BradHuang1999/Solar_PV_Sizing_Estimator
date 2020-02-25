41706b05a22301e0d27c04673a0b663aac4f51f7
Brad Huang <bradhuang1999@yahoo.com>
2020-02-24 22:24:09 -0500

    Add uploading load/pv metrics

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const util = require('util');
+const cp = require('child_process');
+
 const app = express();
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(cors());
+app.use(bodyParser.json({ limit: '2mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const exec = util.promisify(cp.exec);
 
-const cd_folder = 'cd estimation_compiled; ';
+function ab2str(buf) {
+    return String.fromCharCode.apply(null, new Uint16Array(buf));
+}
 
 function process_input(type, promise_sim, promise_snc) {
     return new Promise(resolve => {
@@ -80,39 +86,105 @@ function process_input(type, promise_sim, promise_snc) {
     });
 }
 
-app.get('/', async (req, res) => {
+app.post('/', async (req, res) => {
 
-    const {
+    let {
         estimation_type,
         pv_price_per_kw,
         battery_price_per_kwh,
         epsilon_target,
         confidence_level,
-        days_in_sample
+        days_in_sample,
+        pv_input_type,
+        lat,
+        lon,
+        load_text,
+        pv_text
     } = req.body;
+
+    load_text = load_text.trim();
+    pv_text = pv_text.trim();
+
+    load_len = load_text.split("\n").length;
+    pv_len = pv_text.split("\n").length;
+
+    console.log(
+        estimation_type,
+        pv_price_per_kw,
+        battery_price_per_kwh,
+        epsilon_target,
+        confidence_level,
+        days_in_sample,
+        pv_input_type,
+        lat,
+        lon,
+        load_len,
+        pv_len
+    )
 
     let msg = {};
 
-    if (estimation_type === 'lolp') {
-        input_sim = `${cd_folder} ./sim ${pv_price_per_kw} ${battery_price_per_kwh} 0 ${epsilon_target} ${confidence_level} ${days_in_sample} load.txt pv.txt;`
-        promise_sim = exec(input_sim);
+    function spawn_promise(exec_binary_command, estimation_zero_one) {
+        return new Promise((resolve, reject) => {
+            let args = []
 
-        input_snc = `${cd_folder} ./snc_lolp ${pv_price_per_kw} ${battery_price_per_kwh} ${epsilon_target} ${confidence_level} ${days_in_sample} load.txt pv.txt;`
-        promise_snc = exec(input_snc);
+            args.push(pv_price_per_kw);
+            args.push(battery_price_per_kwh);
+
+            if (estimation_zero_one) {
+                args.push(estimation_zero_one);
+            }
+
+            args.push(epsilon_target);
+            args.push(confidence_level);
+            args.push(days_in_sample);
+
+            if (pv_input_type === "input_file") {
+                args.push("--");
+                args.push(load_len);
+                args.push("--");
+                args.push(pv_len);
+            } else {
+                args.push("estimation_compiled/load.txt");
+                args.push("estimation_compiled/pv.txt");
+            }
+
+            let child = cp.spawn(exec_binary_command, args);
+
+            if (pv_input_type === "input_file") {
+                child.stdin.write(load_text + "\n" + pv_text + "\n");
+            }
+
+            child.stdout.on('data', function (data_buffer) {
+                resolve({
+                    stdout: ab2str(data_buffer)
+                });
+            });
+
+            child.stderr.on('data', function (data_buffer) {
+                resolve({
+                    stderr: ab2str(data_buffer)
+                });
+            });
+        });
+    }
+
+    if (estimation_type === 'lolp') {
+
+        promise_sim = spawn_promise("./estimation_compiled/sim", "0");
+        promise_snc = spawn_promise("./estimation_compiled/snc_lolp");
 
         msg = await process_input('lolp', promise_sim, promise_snc);
 
     } else if (estimation_type == 'eue') {
-        input_sim = `${cd_folder} ./sim ${pv_price_per_kw} ${battery_price_per_kwh} 1 ${epsilon_target} ${confidence_level} ${days_in_sample} load.txt pv.txt;`
-        promise_sim = exec(input_sim);
 
-        input_snc = `${cd_folder} ./snc_eue ${pv_price_per_kw} ${battery_price_per_kwh} ${epsilon_target} ${confidence_level} ${days_in_sample} load.txt pv.txt;`
-        promise_snc = exec(input_snc);
+        promise_sim = spawn_promise("./estimation_compiled/sim", "1");
+        promise_snc = spawn_promise("./estimation_compiled/snc_eue");
 
         msg = await process_input('eue', promise_sim, promise_snc);
 
     } else {
-        msg = {type: 'bad_parameter'};
+        msg = { type: 'bad_parameter' };
     }
 
     console.log(msg);
